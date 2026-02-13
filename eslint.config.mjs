722ab1499378a65657a035a9a7d38f0c76718520
Softgen Dev <dev@softgen.ai>
2026-02-13 16:16:22 +0000

    Edit /eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,26 @@
-
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const baseConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
 
 const eslintConfig = [
-  ...baseConfig,
-  {
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-html-link-for-pages": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-non-null-assertion": "warn",
+    ...baseConfig,
+    {
+        rules: {
+            "react/no-unescaped-entities": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-non-null-assertion": "warn",
+        },
     },
-  },
 ];
 
 export default eslintConfig;
-
