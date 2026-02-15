f4d0693ea3e47abfa4c261b3138a80eba684679e
Softgen Dev <dev@softgen.ai>
2026-02-15 16:06:11 +0000

    Edit /src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,30 @@
 import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import { PWAProvider } from "@/contexts/PWAProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider>
-      <LanguageProvider>
-        <Component {...pageProps} />
-        <Toaster />
-      </LanguageProvider>
-    </ThemeProvider>
-  );
+    return (
+        <>
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#FF6B35" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                <meta name="apple-mobile-web-app-title" content="MaintOps" />
+                <link rel="apple-touch-icon" href="/icons/icon-192.svg" />
+            </Head>
+            <ThemeProvider>
+                <LanguageProvider>
+                    <PWAProvider>
+                        <Component {...pageProps} />
+                        <Toaster />
+                    </PWAProvider>
+                </LanguageProvider>
+            </ThemeProvider>
+        </>
+    );
 }
