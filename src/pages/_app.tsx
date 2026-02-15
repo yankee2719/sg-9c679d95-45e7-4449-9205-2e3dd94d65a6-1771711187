469c88fad408505dcf06ac2d14572b8edd298ae4
Softgen Dev <dev@softgen.ai>
2026-02-15 16:56:04 +0000

    Edit /src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 <meta name="theme-color" content="#FF6B35" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-                <meta name="apple-mobile-web-app-title" content="MaintOps" />
+                <meta name="apple-mobile-web-app-title" content="MACHINA" />
                 <link rel="apple-touch-icon" href="/icons/icon-192.svg" />
             </Head>
             <ThemeProvider>
