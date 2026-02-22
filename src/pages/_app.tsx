baf1293be611d7cf2d83d7f4346ed25746824b21
Softgen Dev <dev@softgen.ai>
2026-02-22 14:19:51 +0000

    Edit /src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,32 +6,29 @@ import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import { PWAProvider } from "@/contexts/PWAProvider";
 
-// App shell (SSR-safe). If a page/component needs browser-only APIs, access them in useEffect.
-function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#FF6B35" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta
-          name="apple-mobile-web-app-status-bar-style"
-          content="black-translucent"
-        />
-        <meta name="apple-mobile-web-app-title" content="MACHINA" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.svg" />
-      </Head>
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="theme-color" content="#FF6B35" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta
+                    name="apple-mobile-web-app-status-bar-style"
+                    content="black-translucent"
+                />
+                <meta name="apple-mobile-web-app-title" content="MACHINA" />
+                <link rel="apple-touch-icon" href="/icons/icon-192.svg" />
+            </Head>
 
-      <ThemeProvider>
-        <LanguageProvider>
-          <PWAProvider>
-            <Component {...pageProps} />
-            <Toaster />
-          </PWAProvider>
-        </LanguageProvider>
-      </ThemeProvider>
-    </>
-  );
+            <ThemeProvider>
+                <LanguageProvider>
+                    <PWAProvider>
+                        <Component {...pageProps} />
+                        <Toaster />
+                    </PWAProvider>
+                </LanguageProvider>
+            </ThemeProvider>
+        </>
+    );
 }
-
-export default App;
