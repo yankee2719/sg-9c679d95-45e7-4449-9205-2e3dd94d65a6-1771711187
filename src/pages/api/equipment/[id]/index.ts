f0ec116460a04d9cc60357312780b9fe7a5c1ecb
Softgen Dev <dev@softgen.ai>
2026-02-26 23:41:48 +0000

    Edit /src/pages/api/equipment/[id]/index.ts

--- src/pages/api/equipment/[id]/index.ts
+++
@@ -1,7 +0,0 @@
-// src/pages/api/equipment/[id]/index.ts
-import type { NextApiRequest, NextApiResponse } from "next";
-import { legacyGone } from "@/lib/legacyApi";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    return legacyGone(req, res);
-}
