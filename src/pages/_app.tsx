a06f9ebcc190f52d6d6e95589f8224916562ca4b
Softgen Dev <dev@softgen.ai>
2026-03-20 23:24:54 +0000

    Edit /src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import "@/styles/globals.css";
 import { OfflineStatusBar } from "@/components/Offline/OfflineStatusBar";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,42 +9,25 @@ import { PWAProvider } from "@/contexts/PWAProvider";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { AuthProvider } from "@/hooks/useAuth";
 
-function AppShell({ Component, pageProps }: AppProps) {
-    const router = useRouter();
-
-    return (
-        <MfaGuard
-            currentPath={router.pathname}
-            excludePaths={[
-                "/login",
-                "/register",
-                "/forgot-password",
-                "/reset-password",
-                "/offline",
-                "/settings/security",
-            ]}
-        >
-            <OfflineStatusBar />
-            <Component {...pageProps} />
-        </MfaGuard>
-    );
-}
-
-export default function App(props: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
+                <title>MACHINA</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
 
             <ThemeProvider>
                 <LanguageProvider>
-                    <AuthProvider>
-                        <PWAProvider>
-                            <AppShell {...props} />
-                            <Toaster />
-                        </PWAProvider>
-                    </AuthProvider>
+                    <PWAProvider>
+                        <AuthProvider>
+                            <MfaGuard>
+                                <OfflineStatusBar />
+                                <Component {...pageProps} />
+                                <Toaster />
+                            </MfaGuard>
+                        </AuthProvider>
+                    </PWAProvider>
                 </LanguageProvider>
             </ThemeProvider>
         </>
