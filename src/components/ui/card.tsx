140284d4d321404a3190d71bcf9447afd5a4b4cb
Softgen Dev <dev@softgen.ai>
2026-03-21 19:21:40 +0000

    Edit /src/components/ui/card.tsx

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
 const Card = React.forwardRef < HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
@@ -24,22 +23,25 @@ const Card = React.forwardRef < HTMLDivElement, React.HTMLAttributes<HTMLDivElem
         );
         CardHeader.displayName = "CardHeader";
 
-        const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
+        const CardTitle = React.forwardRef<HTMLHeadingElement, React.HTMLAttributes<HTMLHeadingElement>>(
             ({className, ...props }, ref) => (
             <h3 ref={ref} className={cn("font-semibold leading-none tracking-tight", className)} {...props} />
             )
             );
             CardTitle.displayName = "CardTitle";
 
-            const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
-                ({className, ...props }, ref) => (
+            const CardDescription = React.forwardRef<
+                HTMLParagraphElement,
+            React.HTMLAttributes<HTMLParagraphElement>
+>(({className, ...props }, ref) => (
                 <p ref={ref} className={cn("text-sm text-muted-foreground", className)} {...props} />
-                )
-                );
+                ));
                 CardDescription.displayName = "CardDescription";
 
                 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-                    ({className, ...props }, ref) => <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+                    ({className, ...props }, ref) => (
+                    <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+                    )
                     );
                     CardContent.displayName = "CardContent";
 
